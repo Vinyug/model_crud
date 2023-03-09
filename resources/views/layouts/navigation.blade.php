1097d9ae1b753fb2513ba9e960ae45029ac0d187
Vinyug <vincentguyomarch89@gmail.com>
2023-03-09 18:38:45 +0100

    Views

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,11 @@
                         {{ __('Liste des utilisateurs') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('roles.index')" :active="request()->routeIs('roles.index')">
+                        {{ __('Rôles') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
